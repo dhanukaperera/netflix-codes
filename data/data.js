b246149c8b01bc7fac0117c1da7bb55c4118e56d
dhanukaperera <dhanukashehanperera@gmail.com>
2022-05-17 18:55:30 +0100

    add css reset and base project setup

---
+++ data/data.js
@@ -0,0 +1,15 @@
+export const NETFLIX_DATA = [
+	{
+		genre: "Christmas",
+		codes: [
+			{
+				code: 1527064,
+				name: "British christmas children & family films",
+			},
+			{
+				code: 1721544,
+				name: "Canadian christmas children & family films",
+			},
+		],
+	},
+];
